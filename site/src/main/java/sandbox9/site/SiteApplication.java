36d3549346731b67ef5f40ee3b3e731e1ecb38e5
kyjin <yours012@gmail.com>
2014-12-24 17:50:11 +0900

    war 배포를 위한 샘플코드 구현

--- site/src/main/java/sandbox9/site/SiteApplication.java
+++ site/src/main/java/sandbox9/site/SiteApplication.java
@@ -2,13 +2,17 @@ package sandbox9.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 @ComponentScan(basePackages = {"sandbox9.site"})
 @EnableAutoConfiguration
-public class SiteApplication {
-	
+public class SiteApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SiteApplication.class, args);
     }
